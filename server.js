d9ac38d1e97364f9e8869914207e9dcd0a8dd3c4
UtkarshCodeCom <yushkarsh@gmail.com>
2024-05-19 13:41:27 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const connectToMongoDB = require('./db');
+const authRoutes = require('./routes/auth');
+const protectedRoute = require('./routes/protectedroutes');
+const path = require('path');
+const cors = require('cors'); // Import the cors middleware
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware to parse JSON bodies
+app.use(express.json());
+
+// Enable CORS for all routes
+app.use(cors());
+
+// Connect to MongoDB
+connectToMongoDB()
+  .then(() => {
+    // Define routes
+    app.use('/api/auth', authRoutes);
+    app.use('/api', protectedRoute);
+    app.use(express.static(path.join(__dirname, "./build")));
+
+    app.get("*", (req, res) => {
+      res.sendFile(path.resolve(__dirname, "./build/index.html"));
+    });
+
+    // Start the server
+    app.listen(PORT, () => {
+      console.log(`Server is running on http://localhost:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+    process.exit(1); // Exit the process with error code 1
+  });
